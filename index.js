c822a60b78ad296ff4a543d62841cddedc2d523c
veroniquechassan@gmail.com <veroniquechassan@gmail.com>
2022-12-19 13:53:45 +0400

    1er commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL =
+  "mongodb+srv://veronique-c:5qqEIbcML920VD43@cluster0.ltzplr1.mongodb.net/test";
+const databaseName = "rest-api";
+
+MongoClient.connect(
+  connectionURL,
+  { useNewUrlParser: true },
+  (error, client) => {
+    if (error) {
+      return console.log("unable to connect to database");
+    }
+    console.log("connected correctly");
+  }
+);
+
+console.log("hello world");
